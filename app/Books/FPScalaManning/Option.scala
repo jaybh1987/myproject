ddac3b07a85491e06d5ba9d23eefdb042d6d1b65
Laitmatus <jaybhavsar497@gmail.com>
2019-09-25 08:06:54 +0530

    option functions cleared.

--- app/Books/FPScalaManning/Option.scala
+++ app/Books/FPScalaManning/Option.scala
@@ -1,8 +1,35 @@
 package Books.FPScalaManning
 
-sealed trait Option[+A]
+sealed trait Option[+A]{
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(value) =>
+    case None => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(value) => f(value)
+    case None => None
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(value) => value
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(value) => ob
+    case None => None
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(value) => if(f(value)) Some(value) else None
+    case None => None
+  }
+}
 case class Some[A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+
 
 
 
