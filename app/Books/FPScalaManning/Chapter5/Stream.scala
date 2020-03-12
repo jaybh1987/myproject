f832a0109ffbf26780253a4f031b7546017c0590
jay bhavsar <jay.bhavsar@laitmatus.com>
2020-03-12 17:25:25 +0530

    Stream object is ready for chapter 5.

---
+++ app/Books/FPScalaManning/Chapter5/Stream.scala
@@ -0,0 +1,19 @@
+package Books.FPScalaManning.Chapter5
+
+sealed trait Stream[+A]
+case object Empty extends Stream[Nothing]
+case class Cons[+A]( h: () => A, t:() => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  def cons[A](hd: => A, t1: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = t1
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if(as.isEmpty) empty else cons(as.head, apply(as.tail:_*) )
+
+}
